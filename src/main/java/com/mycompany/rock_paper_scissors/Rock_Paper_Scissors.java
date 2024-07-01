76c8ac7e44a2e711ec687ad2994fbe8620a3cd9c
Abhilash <abhilashdas092@gmail.com>
2024-07-01 08:38:04 +0530

    java_netBeans

---
+++ src/main/java/com/mycompany/rock_paper_scissors/Rock_Paper_Scissors.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.rock_paper_scissors;
+
+/**
+ *
+ * @author abhil
+ */
+public class Rock_Paper_Scissors {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
